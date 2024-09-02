0a160622679f1e810c6265b7f4873e093f58d03e
Andre Nascimento <1914andre@gmail.com>
2024-09-02 18:36:48 -0300

    feat:adicionando use state + tag Main ao projeto

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    
+    <main>
+        
+    </main>
   )
 }
 
