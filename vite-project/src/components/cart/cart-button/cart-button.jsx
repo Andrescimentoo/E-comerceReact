2d986fd49f0ad9a222c2e24debea98c285e0568a
Andre Nascimento <1914andre@gmail.com>
2024-09-17 22:15:00 -0300

    feat:estruturando base do component cart ao projeto

---
+++ vite-project/src/components/cart/cart-button/cart-button.jsx
@@ -0,0 +1,13 @@
+//AQUI eh a estrutura 
+
+import React from "react";
+
+
+
+ export const ButtonCart = () => {
+    return(
+        <button type="button" className="button-cart">
+           <FaCartPlus/>
+        </button>
+    )
+}
