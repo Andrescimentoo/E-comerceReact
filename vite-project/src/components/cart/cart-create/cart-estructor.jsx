58f822d08b177a25d73c5100989719380eb6b9d8
Andre Nascimento <1914andre@gmail.com>
2024-09-17 22:16:24 -0300

    feat:adicionando estrutura do component do carrinho/Cart

---
+++ vite-project/src/components/cart/cart-create/cart-estructor.jsx
@@ -0,0 +1,24 @@
+//AQUI vou montar a logica do cart
+
+//quero que tenha um botao nos meus carts e ao clicar no botao
+//add o cart ao meu carrinho
+//entao tenho que criar a logica pra passar os carts
+//pra dentro do carrinho
+
+
+//pesquisa aí como fazer
+
+import { ButtonCart } from "../cart-button/cart-button"
+
+
+const Cart = ()=>{
+
+    return(
+        <div className="main-cart">
+           <div className="component-buttom">
+               <ButtonCart/> 
+           </div>
+            
+        </div>
+    )
+}
